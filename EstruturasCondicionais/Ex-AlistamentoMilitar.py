49723663b22a1a12be76c6655df11270f923ca6f
Paulo Vinicius Da Silva <142350879+PauloViniciusDaSilva@users.noreply.github.com>
2023-09-06 00:28:37 -0300

    Ex-AlistamentoMilitar.py
    Formatando as palavras.

--- EstruturasCondicionais/Ex-AlistamentoMilitar.py
+++ EstruturasCondicionais/Ex-AlistamentoMilitar.py
@@ -1,17 +1,19 @@
-ano = int(input('Digite o Ano que você nasceu: '))
-idade= (2023 - ano)
-if idade>18:
+ano = int(input('Digite o ano em que você nasceu: '))
+idade = 2023 - ano
+
+if idade > 18:
     alist = ano + 18
-    falta = ( idade-18)
+    falta = idade - 18
     print(f'Você nasceu em {ano} e tem {idade} anos em 2023.')
-    print(f'Você já se alistouu e deveria se alistar há {falta} anos e seu alistamento foi em {alist}.')
+    print(f'Você já se alistou e deveria ter se alistado há {falta} anos. Seu alistamento foi em {alist}.')
 
 elif idade < 18:
     alist = ano + 18
-    falta = (18 - idade)
+    falta = 18 - idade
     print(f'Você nasceu em {ano} e tem {idade} anos em 2023.')
-    print(f'Falta{falta} anos para vc fazer alistamento.')
-    print(f'Seu alistamento será daqui {alist} anos.')
-elif idade ==18:
-    print(f'Você nasceu em{ano} e tem {idade} anos em 2023.')
-    print('Você deve se alistar rapidamente.')
+    print(f'Faltam {falta} anos para você se alistar.')
+    print(f'Seu alistamento será daqui a {alist} anos.')
+
+elif idade == 18:
+    print(f'Você nasceu em {ano} e tem {idade} anos em 2023.')
+    print('Você deve se alistar imediatamente.')
