20af3f89d91f05bddd1490fa09425e3acfce855a
Paulo Vinicius Da Silva <142350879+PauloViniciusDaSilva@users.noreply.github.com>
2023-10-14 12:44:10 -0300

    Ex-SomaMúltiplos.py
    Olá!! Eu fiz um programa que calcula a soma entre todos os números impares que são múltiplos de três e que se encontram no intervalo de 1 até 500.

---
+++ EstruturaRepetição/Ex-SomaMultiplos.py
@@ -0,0 +1,7 @@
+soma = 0
+con = 0
+for i in range (1,500):
+    if i %2 ! = 0 and i%3 == 0:
+        soma += i
+        con += 1
+print('A soma de todos {}  valores solicitados é {}'.format(con,soma))
